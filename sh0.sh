a7bf9d9faa3b1096fd033166a10a96789b0b2e06
dual4090 <dual4090>
2024-11-30 09:11:40 +0800

    gen-validation

--- sh0.sh
+++ sh0.sh
@@ -1,11 +1,15 @@
 # Description: Generate greedy responses on SQuAD-test
 cuda=0
 models=("Qwen/Qwen2.5-7B-Instruct")
+
 datasets=("squad" "triviaqa")
+splits=("test" "validation")
 for model in "${models[@]}"; do
     for dataset in "${datasets[@]}"; do
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --irrelevant_context --return_latent
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --no-irrelevant_context --return_latent
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --no-use_context --irrelevant_context --return_latent
+        for split in "${splits[@]}"; do
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --irrelevant_context --return_latent
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --no-irrelevant_context --return_latent
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --no-use_context --irrelevant_context --return_latent
+        done
     done
 done
--- sh1.sh
+++ sh1.sh
@@ -1,11 +1,15 @@
 # Description: Generate greedy responses on SQuAD-test
 cuda=1
 models=("meta-llama/Llama-3.1-8B-Instruct")
+
 datasets=("squad" "triviaqa")
+splits=("test" "validation")
 for model in "${models[@]}"; do
     for dataset in "${datasets[@]}"; do
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --irrelevant_context --return_latent
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --no-irrelevant_context --return_latent
-        DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split test --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --no-use_context --irrelevant_context --return_latent
+        for split in "${splits[@]}"; do
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --irrelevant_context --return_latent
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --use_context --no-irrelevant_context --return_latent
+            DEVICE=cuda:${cuda} python generate_responses.py --dataset "${dataset}" --split "${split}" --model "${model}" --num_samples 1000 --num_generations 1 --temperature 0.1 --no-use_context --irrelevant_context --return_latent
+        done
     done
 done
