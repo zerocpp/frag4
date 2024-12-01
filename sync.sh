15b3286701ab1e53b22e22ab0506633e48671253
dual4090 <dual4090>
2024-12-01 12:21:06 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 # 将remote的数据同步到本地
 # rsync -avz song@mbp:/Users/song/code/FRAG/frag3/output/ /Volumes/Fast2T/frag3/output
-watch -n 60 rsync -avz song@d4090:/home/song/code/frag4/output/ /Volumes/Fast2T/frag4/output
+watch -n 60 rsync -avz --sparse song@d4090:/home/song/code/frag4/output/ /Volumes/Fast2T/frag4/output
