1a553021c7735d25a839bc11738e4ce50fbc6644
home@mac <home@mac>
2024-12-03 16:38:38 +0800

    cuda

--- cuda0.sh
+++ cuda0.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Description: Script to generate responses
-
+# GPU-0执行的脚本
 cuda=0
+
+###########################################
+# Description: Script to generate responses
 models=("Qwen/Qwen2.5-7B-Instruct")
 # models=("Qwen/Qwen2.5-7B-Instruct" "meta-llama/Llama-3.1-8B-Instruct")
 
@@ -16,13 +18,20 @@ latents=("--return_latent" "--no-return_latent")
 datasets=("squad" "triviaqa")
 splits=("train" "test" "validation")
 
-for model in "${models[@]}"; do
-    for dataset in "${datasets[@]}"; do
-        for split in "${splits[@]}"; do
-            for i in "${!context_types[@]}"; do
+# 任务计数器
+task_counter=0
+# 任务总数
+task_total=$((1*2*3*3*2))
+
+for model in "${models[@]}"; do # 1
+    for dataset in "${datasets[@]}"; do # 2
+        for split in "${splits[@]}"; do # 3
+            for i in "${!context_types[@]}"; do # 3
                 context_type="${context_types[$i]}"
                 context="${contexts[$i]}"
-                for j in "${!sample_prefixs[@]}"; do
+                for j in "${!sample_prefixs[@]}"; do # 2
+                    task_counter=$((task_counter + 1))
+                    echo "Task1: $task_counter/$task_total"
                     sample_prefix="${sample_prefixs[$j]}"
                     num_generation="${num_generations[$j]}"
                     temperature="${temperatures[$j]}"
@@ -34,6 +43,40 @@ for model in "${models[@]}"; do
                     echo "> $cmd"
                     eval $cmd
                 done
+            done
+        done
+    done
+done
+
+
+
+###########################################
+# Description: Script to cluster responses
+models=("Qwen/Qwen2.5-7B-Instruct")
+datasets=("squad" "triviaqa")
+splits=("train" "test" "validation")
+sample_suffixs=("irrelevant" "golden" "without")
+override="--no-override"
+root_dir="."
+
+# 任务计数器
+task_counter=0
+# 任务总数
+task_total=$((1*2*3*3))
+
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        for split in "${splits[@]}"; do
+            for sample_suffix in "${sample_suffixs[@]}"; do
+                task_counter=$((task_counter + 1))
+                echo "Task2: $task_counter/$task_total"
+                sample="sample_${sample_suffix}"
+                dataset_json_file="$root_dir/output/dataset/${dataset}_${split}.json"
+                input_dir="$root_dir/output/${split}/generation/${model}/${dataset}/${sample}"
+                output_dir="$root_dir/output/${split}/clustered/${model}/${dataset}/${sample}"
+                cmd="DEVICE=cuda:$cuda python cluster_responses.py --dataset_json_file $dataset_json_file --input_dir $input_dir --output_dir $output_dir $override"
+                echo "> $cmd"
+                eval $cmd
             done
         done
     done
--- cuda1.sh
+++ cuda1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Description: Script to generate responses
-
+# GPU-1执行的脚本
 cuda=1
+
+###########################################
+# Description: Script to generate responses
 models=("meta-llama/Llama-3.1-8B-Instruct")
 # models=("Qwen/Qwen2.5-7B-Instruct" "meta-llama/Llama-3.1-8B-Instruct")
 
@@ -16,6 +18,11 @@ latents=("--return_latent" "--no-return_latent")
 datasets=("squad" "triviaqa")
 splits=("train" "test" "validation")
 
+# 任务计数器
+task_counter=0
+# 任务总数
+task_total=$((1*2*3*3*2))
+
 for model in "${models[@]}"; do
     for dataset in "${datasets[@]}"; do
         for split in "${splits[@]}"; do
@@ -23,6 +30,8 @@ for model in "${models[@]}"; do
                 context_type="${context_types[$i]}"
                 context="${contexts[$i]}"
                 for j in "${!sample_prefixs[@]}"; do
+                    task_counter=$((task_counter + 1))
+                    echo "Task1: $task_counter/$task_total"
                     sample_prefix="${sample_prefixs[$j]}"
                     num_generation="${num_generations[$j]}"
                     temperature="${temperatures[$j]}"
@@ -34,6 +43,39 @@ for model in "${models[@]}"; do
                     echo "> $cmd"
                     eval $cmd
                 done
+            done
+        done
+    done
+done
+
+
+###########################################
+# Description: Script to cluster responses
+models=("meta-llama/Llama-3.1-8B-Instruct")
+datasets=("squad" "triviaqa")
+splits=("train" "test" "validation")
+sample_suffixs=("irrelevant" "golden" "without")
+override="--no-override"
+root_dir="."
+
+# 任务计数器
+task_counter=0
+# 任务总数
+task_total=$((1*2*3*3))
+
+for model in "${models[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        for split in "${splits[@]}"; do
+            for sample_suffix in "${sample_suffixs[@]}"; do
+                task_counter=$((task_counter + 1))
+                echo "Task2: $task_counter/$task_total"
+                sample="sample_${sample_suffix}"
+                dataset_json_file="$root_dir/output/dataset/${dataset}_${split}.json"
+                input_dir="$root_dir/output/${split}/generation/${model}/${dataset}/${sample}"
+                output_dir="$root_dir/output/${split}/clustered/${model}/${dataset}/${sample}"
+                cmd="DEVICE=cuda:$cuda python cluster_responses.py --dataset_json_file $dataset_json_file --input_dir $input_dir --output_dir $output_dir $override"
+                echo "> $cmd"
+                eval $cmd
             done
         done
     done
